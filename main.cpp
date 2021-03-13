1de76470a6258b5c4cbbe9021fefbf9c99cd5d98
Lenspace <ustbenspace@gmail.com>
2021-03-13 17:21:33 +0000

    test rapidJson build in project

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 using namespace std;
 #include "globalFunc.h"
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
+using namespace rapidjson;
 
 int main()
 {
     cout << "hello world." << endl;
     cout << "12 + 22 = " << add(12, 22) << endl;
+
+    const char* json = "{\"project\" : \"rapidJson\", \"stars\" : 10}";
+    Document d;
+    d.Parse(json);
+
+    Value& s = d["stars"];
+    s.SetInt(s.GetInt() + 1);
+
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    d.Accept(writer);
+
+    cout << buffer.GetString() << endl;
+
     return 0;
 }
