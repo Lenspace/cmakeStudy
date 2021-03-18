2eab2cfb9535202746a42be5eed65571df697c49
Lenspace <ustbenspace@gmail.com>
2021-03-18 10:22:40 +0000

    test webSocket cs

--- ws-server/echo_server.cpp
+++ ws-server/echo_server.cpp
@@ -1,7 +1,7 @@
 // examles from websocketpp
 // url: https://github.com/zaphoyd/websocketpp
 
-#define ASIO_STANDALONE
+
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
 
